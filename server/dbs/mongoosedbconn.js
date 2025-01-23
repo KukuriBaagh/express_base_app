5183d9e21a7cea0bcbe57184a3b62d1ccaddafa0
KukuriBaagh <howtoccdemo@gmail.com>
2025-01-23 21:38:29 +0530

    scha

--- server/dbs/mongoosedbconn.js
+++ server/dbs/mongoosedbconn.js
@@ -1,3 +1,18 @@
 import mongoose from 'mongoose';
 
-const connectDB = mongoose.connect();
+// step 1 to connect to mongodb server
+
+try {
+  await mongoose.connect('mongodb://localhost:27017/');
+  mongoose.set('debug', true);
+} catch (error) {
+  console.error(error);
+  process.exit();
+}
+
+// step 2: create schema
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
